e50ed173d3149e501c2d7cbcb4c9acb5528d812e
Patryk Grabowski <grabowskip@icloud.com>
2025-11-21 23:11:05 +0100

    fix: hyprland fixes so it's usable

--- home-manager/modules/hyprland.nix
+++ home-manager/modules/hyprland.nix
@@ -7,10 +7,9 @@
     settings = {
       "$mainMod" = "SUPER";
 
-      # monitor = [
-      #   "DP-3,3840x2160,0x0,1.5"
-	    #   "HDMI-2,3840x2160,0x-3840,2"
-      # ];
+      monitor = [
+        "HDMI-A-1,3440x1440,0x0,1.5"
+      ];
 
       xwayland = {
 	      force_zero_scaling = 1;
@@ -112,112 +111,111 @@
       #   "float, ^(mpv)$"
       # ];
 
-      # exec-once = [
-      #   "swww init"
-      #   "swww img ./nixos-wallpaper.webp"
-      #   "waybar"
-      #   "wl-paste --type text --watch cliphist store"
-      #   "wl-paste --type image --watch cliphist store"
-      # ];
+      exec-once = [
+        "swww init"
+        "swww img ./nixos-wallpaper.webp"
+        "waybar"
+        "wl-paste --type text --watch cliphist store"
+        "wl-paste --type image --watch cliphist store"
+      ];
 
-      # workspace = [
-	    #   "1, monitor:DP-3"
-	    #   "1, monitor:HDMI-2"
-      # ];
+      workspace = [
+	      "1, monitor:HDMI-A-1"
+      ];
 
-      # bind = [
-      #   "$mainMod, V, exec, cliphist list | wofi --dmenu | cliphist decode | wl-copy"
+      bind = [
+        "$mainMod, V, exec, cliphist list | wofi --dmenu | cliphist decode | wl-copy"
 
-      #   "$mainMod, Return, exec, wezterm"
-      #   "$mainMod, Q, killactive,"
-      #   "$mainMod, M, exit,"
-      #   "$mainMod, E, exec, thunar"
-      #   "$mainMod, F, togglefloating,"
-      #   "$mainMod, D, exec, fuzzel --show drun"
-      #   "$mainMod, P, pseudo, # dwindle"
-      #   "$mainMod, J, togglesplit, # dwindle"
-      #   "$mainMod, L, exec, hyprlock"
+        "$mainMod, Return, exec, wezterm"
+        "$mainMod, Q, killactive,"
+        "$mainMod, M, exit,"
+        "$mainMod, E, exec, thunar"
+        "$mainMod, F, togglefloating,"
+        "$mainMod, D, exec, fuzzel --show drun"
+        "$mainMod, P, pseudo, # dwindle"
+        "$mainMod, J, togglesplit, # dwindle"
+        "$mainMod, L, exec, hyprlock"
 
-      #   # Move focus with mainMod + arrow keys
-      #   "$mainMod, left,  movefocus, l"
-      #   "$mainMod, right, movefocus, r"
-      #   "$mainMod, up,    movefocus, u"
-      #   "$mainMod, down,  movefocus, d"
+        # Move focus with mainMod + arrow keys
+        "$mainMod, left,  movefocus, l"
+        "$mainMod, right, movefocus, r"
+        "$mainMod, up,    movefocus, u"
+        "$mainMod, down,  movefocus, d"
 
-      #   # Moving windows
-      #   "$mainMod SHIFT, left,  swapwindow, l"
-      #   "$mainMod SHIFT, right, swapwindow, r"
-      #   "$mainMod SHIFT, up,    swapwindow, u"
-      #   "$mainMod SHIFT, down,  swapwindow, d"
+        # Moving windows
+        "$mainMod SHIFT, left,  swapwindow, l"
+        "$mainMod SHIFT, right, swapwindow, r"
+        "$mainMod SHIFT, up,    swapwindow, u"
+        "$mainMod SHIFT, down,  swapwindow, d"
 
-      #   # Window resizing                     X  Y
-      #   "$mainMod CTRL, left,  resizeactive, -60 0"
-      #   "$mainMod CTRL, right, resizeactive,  60 0"
-      #   "$mainMod CTRL, up,    resizeactive,  0 -60"
-      #   "$mainMod CTRL, down,  resizeactive,  0  60"
+        # Window resizing                     X  Y
+        "$mainMod CTRL, left,  resizeactive, -60 0"
+        "$mainMod CTRL, right, resizeactive,  60 0"
+        "$mainMod CTRL, up,    resizeactive,  0 -60"
+        "$mainMod CTRL, down,  resizeactive,  0  60"
 
-      #   # Switch workspaces with mainMod + [0-9]
-      #   "$mainMod, 1, workspace, 1"
-      #   "$mainMod, 2, workspace, 2"
-      #   "$mainMod, 3, workspace, 3"
-      #   "$mainMod, 4, workspace, 4"
-      #   "$mainMod, 5, workspace, 5"
-      #   "$mainMod, 6, workspace, 6"
-      #   "$mainMod, 7, workspace, 7"
-      #   "$mainMod, 8, workspace, 8"
-      #   "$mainMod, 9, workspace, 9"
-      #   "$mainMod, 0, workspace, 10"
+        # Switch workspaces with mainMod + [0-9]
+        "$mainMod, 1, workspace, 1"
+        "$mainMod, 2, workspace, 2"
+        "$mainMod, 3, workspace, 3"
+        "$mainMod, 4, workspace, 4"
+        "$mainMod, 5, workspace, 5"
+        "$mainMod, 6, workspace, 6"
+        "$mainMod, 7, workspace, 7"
+        "$mainMod, 8, workspace, 8"
+        "$mainMod, 9, workspace, 9"
+        "$mainMod, 0, workspace, 10"
 
-      #   # Move active window to a workspace with mainMod + SHIFT + [0-9]
-      #   "$mainMod SHIFT, 1, movetoworkspacesilent, 1"
-      #   "$mainMod SHIFT, 2, movetoworkspacesilent, 2"
-      #   "$mainMod SHIFT, 3, movetoworkspacesilent, 3"
-      #   "$mainMod SHIFT, 4, movetoworkspacesilent, 4"
-      #   "$mainMod SHIFT, 5, movetoworkspacesilent, 5"
-      #   "$mainMod SHIFT, 6, movetoworkspacesilent, 6"
-      #   "$mainMod SHIFT, 7, movetoworkspacesilent, 7"
-      #   "$mainMod SHIFT, 8, movetoworkspacesilent, 8"
-      #   "$mainMod SHIFT, 9, movetoworkspacesilent, 9"
-      #   "$mainMod SHIFT, 0, movetoworkspacesilent, 10"
+        # Move active window to a workspace with mainMod + SHIFT + [0-9]
+        "$mainMod SHIFT, 1, movetoworkspacesilent, 1"
+        "$mainMod SHIFT, 2, movetoworkspacesilent, 2"
+        "$mainMod SHIFT, 3, movetoworkspacesilent, 3"
+        "$mainMod SHIFT, 4, movetoworkspacesilent, 4"
+        "$mainMod SHIFT, 5, movetoworkspacesilent, 5"
+        "$mainMod SHIFT, 6, movetoworkspacesilent, 6"
+        "$mainMod SHIFT, 7, movetoworkspacesilent, 7"
+        "$mainMod SHIFT, 8, movetoworkspacesilent, 8"
+        "$mainMod SHIFT, 9, movetoworkspacesilent, 9"
+        "$mainMod SHIFT, 0, movetoworkspacesilent, 10"
 
-      #   # Scroll through existing workspaces with mainMod + scroll
-      #   "$mainMod, mouse_down, workspace, e+1"
-      #   "$mainMod, mouse_up, workspace, e-1"
+        # Scroll through existing workspaces with mainMod + scroll
+        "$mainMod, mouse_down, workspace, e+1"
+        "$mainMod, mouse_up, workspace, e-1"
 
-      #   # Keyboard backlight
-      #   "$mainMod, F3, exec, brightnessctl -d *::kbd_backlight set +33%"
-      #   "$mainMod, F2, exec, brightnessctl -d *::kbd_backlight set 33%-"
+        # Keyboard backlight
+        "$mainMod, F3, exec, brightnessctl -d *::kbd_backlight set +33%"
+        "$mainMod, F2, exec, brightnessctl -d *::kbd_backlight set 33%-"
 
-      #   # Volume and Media Control
-      #   ", XF86AudioRaiseVolume, exec, pamixer -i 5 "
-      #   ", XF86AudioLowerVolume, exec, pamixer -d 5 "
-      #   ", XF86AudioMute, exec, pamixer -t"
-      #   ", XF86AudioMicMute, exec, pamixer --default-source -t"
+        # Volume and Media Control
+        ", XF86AudioRaiseVolume, exec, pamixer -i 5 "
+        ", XF86AudioLowerVolume, exec, pamixer -d 5 "
+        ", XF86AudioMute, exec, pamixer -t"
+        ", XF86AudioMicMute, exec, pamixer --default-source -t"
         
-      #   # Brightness control
-      #   ", XF86MonBrightnessDown, exec, brightnessctl set 5%- "
-      #   ", XF86MonBrightnessUp, exec, brightnessctl set +5% "
+        # Brightness control
+        ", XF86MonBrightnessDown, exec, brightnessctl set 5%- "
+        ", XF86MonBrightnessUp, exec, brightnessctl set +5% "
 
-      #   # Configuration files
-      #   ''$mainMod SHIFT, N, exec, alacritty -e sh -c "rb"''
-      #   ''$mainMod SHIFT, C, exec, alacritty -e sh -c "conf"''
-      #   ''$mainMod SHIFT, H, exec, alacritty -e sh -c "nvim ~/nix/home-manager/modules/hyprland.nix"''
-      #   ''$mainMod SHIFT, W, exec, alacritty -e sh -c "nvim ~/nix/home-manager/modules/waybar.nix''
-      #   '', Print, exec, grim -g "$(slurp)" - | swappy -f -''
+        # Configuration files
+        ''$mainMod SHIFT, N, exec, alacritty -e sh -c "rb"''
+        ''$mainMod SHIFT, C, exec, alacritty -e sh -c "conf"''
+        ''$mainMod SHIFT, H, exec, alacritty -e sh -c "nvim ~/nix/home-manager/modules/hyprland.nix"''
+        ''$mainMod SHIFT, W, exec, alacritty -e sh -c "nvim ~/nix/home-manager/modules/waybar.nix''
+        '', Print, exec, grim -g "$(slurp)" - | swappy -f -''
 
-      #   # Waybar
-      #   "$mainMod, B, exec, pkill -SIGUSR1 waybar"
-      #   "$mainMod, W, exec, pkill -SIGUSR2 waybar"
+        # Waybar
+        "$mainMod, B, exec, pkill -SIGUSR1 waybar"
+        "$mainMod, W, exec, pkill -SIGUSR2 waybar"
 
-      #   # Disable all effects
-      #   "$mainMod Shift, G, exec, ~/.config/hypr/gamemode.sh "
-      # ];
+        # Disable all effects
+        "$mainMod Shift, G, exec, ~/.config/hypr/gamemode.sh "
+      ];
 
       # Move/resize windows with mainMod + LMB/RMB and dragging
-      # bindm = [
-      #   "$mainMod, mouse:272, movewindow"
-      #   "$mainMod, mouse:273, resizewindow"
-      # ];
+      bindm = [
+        "$mainMod, mouse:272, movewindow"
+        "$mainMod, mouse:273, resizewindow"
+      ];
     };
   };
 }
